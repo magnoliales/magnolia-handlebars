5092a905f4198ac863f7a00700b19cb23c233978
vasily-kartashov <info@kartashov.com>
2014-09-23 12:57:18 +0100

    helpers now connected through handlebars settings in the modules

--- magnolia-handlebars/src/main/java/com/magnoliales/handlebars/renderer/HandlebarsRenderer.java
+++ magnolia-handlebars/src/main/java/com/magnoliales/handlebars/renderer/HandlebarsRenderer.java
@@ -13,6 +13,8 @@ import com.github.jknack.handlebars.io.CompositeTemplateLoader;
 import com.github.jknack.handlebars.io.FileTemplateLoader;
 import com.github.jknack.handlebars.io.TemplateLoader;
 import info.magnolia.cms.core.AggregationState;
+import info.magnolia.context.MgnlContext;
+import info.magnolia.jcr.util.PropertyUtil;
 import info.magnolia.module.blossom.render.RenderContext;
 import info.magnolia.rendering.context.RenderingContext;
 import info.magnolia.rendering.engine.RenderException;
@@ -21,11 +23,15 @@ import info.magnolia.rendering.model.RenderingModel;
 import info.magnolia.rendering.renderer.AbstractRenderer;
 import info.magnolia.rendering.template.RenderableDefinition;
 import info.magnolia.rendering.util.AppendableWriter;
+import info.magnolia.repository.RepositoryConstants;
+import org.apache.jackrabbit.commons.JcrUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import javax.jcr.Node;
+import javax.jcr.Property;
+import javax.jcr.Session;
 import java.io.File;
 import java.util.HashMap;
 import java.util.Locale;
@@ -46,17 +52,21 @@ public class HandlebarsRenderer extends AbstractRenderer {
         );
         handlebars = new Handlebars(loader);
         handlebars.with(new ConcurrentMapTemplateCache());
-    }
 
-    public void setHelpers(Map<String, Class<? extends Helper>> helpers) {
-        for (String helperName : helpers.keySet()) {
-            Class<? extends Helper> helperClass = helpers.get(helperName);
-            try {
-                Helper helper = helperClass.newInstance();
+        // @todo, really not sure why node2bean doesn't work on this one
+        try {
+            Session session = MgnlContext.getJCRSession(RepositoryConstants.CONFIG);
+            Node helpersNode = session.getNode("/modules/handlebars/renderers/handlebars/helpers");
+            for (Node helperNode : JcrUtils.getChildNodes(helpersNode)) {
+                String helperName = PropertyUtil.getString(helperNode, "name");
+                String helperClassName = PropertyUtil.getString(helperNode, "class");
+                log.info("Adding handlebars helper {}: {}", helperName, helperClassName);
+                Class<?> helperClass = Class.forName(helperClassName);
+                Helper helper = (Helper) helperClass.newInstance();
                 handlebars.registerHelper(helperName, helper);
-            } catch (Exception e) {
-                log.error("Cannot instantiate helpers", e);
             }
+        } catch (Exception e) {
+            log.error("Cannot read helpers information", e);
         }
     }
 
