24b1bf13682eabfd84995d7d918437282a4439d6
vkartashov <kartashov.vasily@googlemail.com>
2015-01-26 15:03:40 +0000

    template inheritance now works as well

--- magnolia-handlebars/src/main/java/com/magnoliales/handlebars/mapper/NodeObjectMapperImpl.java
+++ magnolia-handlebars/src/main/java/com/magnoliales/handlebars/mapper/NodeObjectMapperImpl.java
@@ -1,5 +1,6 @@
 package com.magnoliales.handlebars.mapper;
 
+import com.github.jknack.handlebars.internal.js.RhinoHandlebars;
 import com.magnoliales.handlebars.annotations.Page;
 import com.magnoliales.handlebars.annotations.Field;
 import info.magnolia.jcr.util.PropertyUtil;
@@ -18,34 +19,44 @@ public class NodeObjectMapperImpl implements NodeObjectMapper {
 
     @Override
     public Object map(Node node) {
-        String nodeClassName = PropertyUtil.getString(node, "mgnl:template");
         try {
-            return map(Class.forName(nodeClassName), node);
+            String nodeClassName = node.getProperty("mgnl:template").getString();
+            Class<?> type = Class.forName(nodeClassName);
+            Object object = type.newInstance();
+            return map(type, object, node);
         } catch (ClassNotFoundException | RepositoryException | InstantiationException | IllegalAccessException e) {
-            logger.error("Cannot map node {} to class '{}'", node, nodeClassName);
+            logger.error("Cannot map node {}", node);
             return null;
         }
     }
 
-    private Object map(Class<?> type, Node node)
+    private Object map(Class<?> type, Object object, Node node)
             throws IllegalAccessException, InstantiationException, RepositoryException {
 
-        Object object = type.newInstance();
-        for (java.lang.reflect.Field field : type.getDeclaredFields()) {
-            field.setAccessible(true);
-            String fieldName = field.getName();
-            if (field.isAnnotationPresent(Field.class) && node.hasProperty(fieldName)) {
-                Property property = node.getProperty(fieldName);
-                switch (property.getType()) {
-                    case PropertyType.STRING:
-                        field.set(object, property.getString());
-                        break;
-                    default:
-                        throw new AssertionError("Not implemented");
+        while (!type.equals(Object.class)) {
+            for (java.lang.reflect.Field field : type.getDeclaredFields()) {
+                field.setAccessible(true);
+                String fieldName = field.getName();
+                if (field.isAnnotationPresent(Field.class) && node.hasProperty(fieldName)) {
+                    Property property = node.getProperty(fieldName);
+                    switch (property.getType()) {
+                        case PropertyType.STRING:
+                            field.set(object, property.getString());
+                            break;
+                        default:
+                            throw new AssertionError("Not implemented");
+                    }
+                } else if (node.hasNode(fieldName)) {
+                    Object property = field.getType().newInstance();
+                    field.set(object, map(field.getType(), property, node.getNode(fieldName)));
                 }
-            } else if (node.hasNode(fieldName)) {
-                field.set(object, map(field.getType(), node.getNode(fieldName)));
             }
+            type = type.getSuperclass();
+            if (type.isAnnotationPresent(Page.class)) {
+                String supplierId = PropertyUtil.getString(node, "mgnl:supplierPage");
+                node = node.getSession().getNodeByIdentifier(supplierId);
+            }
+            map(type, object, node);
         }
         return object;
     }
